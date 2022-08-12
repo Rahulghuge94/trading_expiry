53d1642abeb5eb4dd8894eeb718186beb435464f
Rahulghuge94 <1994ghuge@gmail.com>
2022-08-12 22:36:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 import setuptools
 
-setuptools.setup(
-    name = "trading_expiry",
-    version = "0.1",
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    description = 'library to get trading expiries.',
-    long_description="library to get trading expiries.",
-    long_description_content_type="text/markdown",  author = 'Rahul',
-    author_email = '1994ghuge.gmail.com',
-    url = 'https://github.com/Rahulghuge94/trading_expiry',
-    keywords = ['NSE', 'OPTION', 'FUTURE' 'python', 'sdk', 'trading', 'stock markets'],
-    classifiers=[
-      'Intended Audience :: Developers',
-      'Natural Language :: English',
-      'Operating System :: OS Independent',
-      'Programming Language :: Python',
-      'Programming Language :: Python :: 3.6'])
+
+setuptools.setup(name = "trading_expiry",
+      version='0.1',
+      description='library to get trading expiries.',
+      url = 'https://github.com/Rahulghuge94/trading_expiry',
+      author='Rahul Ghuge',
+      packages=['trading_expiry'],
+      author_email='1994ghuge.gmail.com',
+      license='MIT',
+      )
