49c9029af7d507cbf8439a9b1ce796009ee084f3
Rahulghuge94 <1994ghuge@gmail.com>
2022-08-12 22:19:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-import setuptool
+from setuptools import setup
+
+setup(
+    name = "trading_expiry",
+    version = "0.1",
+    author = "Rahul Ghuge",
+    author_email = "andrewjcarter@gmail.com",
+    description = ("Library to get traing expiries."),
+    keywords = "NSE-FO expiries",
+    url = "http://packages.python.org/an_example_pypi_project",
+    long_description="library to get trading expiry",
+)
